b040ad564c53a8bf6f8bcbb62c3a8c1d0f821091
qutaiba25 <qutaiba25>
2025-07-23 10:26:34 +0300

    update

--- src/tests/UserTests.java
+++ src/tests/UserTests.java
@@ -12,71 +12,76 @@ import utils.TestDataGenerator;
 import java.util.List;
 
 public class UserTests {
-    WebDriver driver;
-    SignupPage signupPage;
-    String firstName;
-    String username;
-    String password = "Test@1234";
+	WebDriver driver;
+	SignupPage signupPage;
+	String firstName;
+	String username;
+	String password = "Test@1234";
 
-    @BeforeTest
-    public void setup() {
-        driver = DriverFactory.getDriver();
-        driver.get("https://automationteststore.com/index.php?rt=account/create");
-        signupPage = new SignupPage(driver);
-    }
+	@BeforeTest
+	public void setup() {
+		driver = DriverFactory.getDriver();
+		driver.get("https://automationteststore.com/index.php?rt=account/create");
+		signupPage = new SignupPage(driver);
+	}
 
-    @Test(priority = 1)
-    public void testSignup() throws InterruptedException {
-        firstName = TestDataGenerator.getRandomFirstName();
-        String lastName = TestDataGenerator.getRandomLastName();
-        username = TestDataGenerator.getRandomUsername(firstName, lastName);
-        String email = TestDataGenerator.getEmail(firstName, lastName);
+	@Test(priority = 1)
+	public void testSignup() throws InterruptedException {
+		firstName = TestDataGenerator.getRandomFirstName();
+		String lastName = TestDataGenerator.getRandomLastName();
+		username = TestDataGenerator.getRandomUsername(firstName, lastName);
+		String email = TestDataGenerator.getEmail(firstName, lastName);
 
-        signupPage.fillForm(firstName, lastName, email, "9624545455", username, password);
-        Assert.assertTrue(signupPage.isSignupSuccess(), "Signup failed");
-    }
+		signupPage.fillForm(firstName, lastName, email, "9624545455", username, password);
+		Assert.assertTrue(signupPage.isSignupSuccess(), "Signup failed");
+	}
 
-    @Test(priority = 3, dependsOnMethods = {"testSignup"})
-    public void testLogin() throws InterruptedException {
-        driver.findElement(By.partialLinkText("Login or register")).click();
-        driver.findElement(By.id("loginFrm_loginname")).sendKeys(username);
-        driver.findElement(By.id("loginFrm_password")).sendKeys(password);
-        driver.findElement(By.xpath("//button[@title='Login']")).click();
-        Thread.sleep(1000);
-        Assert.assertTrue(driver.findElement(By.id("customernav")).getText().contains(firstName), "Login failed");
-    }
+	@Test(priority = 3, dependsOnMethods = { "testSignup" })
+	public void testLogin() throws InterruptedException {
+		driver.findElement(By.partialLinkText("Login or register")).click();
+		driver.findElement(By.id("loginFrm_loginname")).sendKeys(username);
+		driver.findElement(By.id("loginFrm_password")).sendKeys(password);
+		driver.findElement(By.xpath("//button[@title='Login']")).click();
+		Thread.sleep(1000);
+		Assert.assertTrue(driver.findElement(By.id("customernav")).getText().contains(firstName), "Login failed");
+	}
 
+	@Test(priority = 2)
+	public void testLogout() throws InterruptedException {
+		WebElement logoutButton = driver.findElement(By.linkText("Logoff"));
+		logoutButton.click();
+		Thread.sleep(1000);
+		Assert.assertTrue(driver.getPageSource().contains("You have been logged off your account."), "Logout failed");
+	}
 
-    @Test(priority = 2)
-    public void testLogout() throws InterruptedException {
-        WebElement logoutButton = driver.findElement(By.linkText("Logoff"));
-        logoutButton.click();
-        Thread.sleep(1000);
-        Assert.assertTrue(driver.getPageSource().contains("You have been logged off your account."), "Logout failed");
-    }
-    
+	@Test(priority = 4, enabled = true)
+	public void testAddToCartAndCheckout() throws InterruptedException {
+		driver.get("https://automationteststore.com/");
+		Thread.sleep(1000);
+		List<WebElement> items = driver.findElements(By.className("prdocutname"));
+		items.get((int) (Math.random() * items.size())).click();
+		Thread.sleep(2000);
+//		 WebElement addToCartBtn = driver.findElement(By.className("productpagecart"));
+//		 return;
+		WebElement addToCartBtn = driver.findElement(By.className("productpagecart"));
+		if (addToCartBtn.getText().equals("Out of Stock")) {
+			System.out.println("Item is out of stock");
+			return;
+		} else {
+			if (driver.getCurrentUrl().contains("product_id=116")) {
+				driver.findElement(By.xpath("//label[@for='option344747']")).click();
+			}
+			addToCartBtn.click();
+			Thread.sleep(1000);
+		}
+//		addToCartBtn.click();
+//		Thread.sleep(1000);
+		driver.findElement(By.linkText("Checkout")).click();
+		Assert.assertTrue(driver.getCurrentUrl().contains("checkout"), "Checkout navigation failed");
+	}
 
-
-    @Test(priority = 4, enabled = true)
-    public void testAddToCartAndCheckout() throws InterruptedException {
-        driver.get("https://automationteststore.com/");
-        Thread.sleep(1000);
-        List<WebElement> items = driver.findElements(By.className("prdocutname"));
-        items.get((int)(Math.random() * items.size())).click();
-        Thread.sleep(2000);
-        WebElement addToCartBtn = driver.findElement(By.className("productpagecart"));
-        if (addToCartBtn.getText().equals("Out of Stock")) {
-            System.out.println("Item is out of stock");
-            return;
-        }
-        addToCartBtn.click();
-        Thread.sleep(1000);
-        driver.findElement(By.linkText("Checkout")).click();
-        Assert.assertTrue(driver.getCurrentUrl().contains("checkout"), "Checkout navigation failed");
-    }
-
-    @AfterTest
-    public void teardown() {
-        DriverFactory.quitDriver();
-    }
+	@AfterTest
+	public void teardown() {
+		DriverFactory.quitDriver();
+	}
 }
